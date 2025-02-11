665b2b2709d4db2b270fc482735603fff3bd2e2c
Denish Navadiya <denishnavadiya1@gmail.com>
2025-02-11 15:17:29 +0530

    feat: add popover installation guidelines

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -148,18 +148,14 @@ export const DropdownMenuSubTrigger = ({
 
 export type DropdownMenuSubContentProps = React.ComponentProps<typeof DropdownMenuPrimitive.SubContent>;
 
-const menuContentClasses = ["bg-popover text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded border p-1 shadow-lg",
+const menuContentClasses = [
+    "bg-popover text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded border p-1 shadow-lg",
     "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-    "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"]
+    "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+];
 
 export const DropdownMenuSubContent = ({ className, ...props }: DropdownMenuSubContentProps) => (
-    <DropdownMenuPrimitive.SubContent
-        className={cn(
-            ...menuContentClasses,
-            className,
-        )}
-        {...props}
-    />
+    <DropdownMenuPrimitive.SubContent className={cn(...menuContentClasses, className)} {...props} />
 );
 
 export type DropdownMenuContentProps = React.ComponentProps<typeof DropdownMenuPrimitive.Content>;
@@ -168,11 +164,7 @@ export const DropdownMenuContent = ({ className, sideOffset = 4, ...props }: Dro
     <DropdownMenuPrimitive.Portal>
         <DropdownMenuPrimitive.Content
             sideOffset={sideOffset}
-            className={cn(
-                ...menuContentClasses,
-                "space-y-0.5",
-                className,
-            )}
+            className={cn(...menuContentClasses, "space-y-0.5", className)}
             {...props}
         />
     </DropdownMenuPrimitive.Portal>
