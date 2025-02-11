bea787677943bfe1a7727eb878ef75ea9c740f69
Denish Navadiya <denishnavadiya1@gmail.com>
2025-02-11 12:50:30 +0530

    feat: add alert installation guidelines

---
+++ website/components/docs/clipboard-button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { CheckIcon, ClipboardIcon } from "lucide-react";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { copyToClipboard } from "@/website/utils/clipboard";
+
+export const ClipboardButton = ({ text }: { text: string }) => {
+    const [hasCopied, setHasCopied] = useState(false);
+
+    const copy = () => {
+        setHasCopied(true);
+        copyToClipboard(text);
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 3000);
+    };
+
+    return (
+        <Button
+            variant="ghost"
+            color="secondary"
+            className="relative rounded-lg text-white hover:bg-white/25"
+            size="sm"
+            aria-label="Copy"
+            onClick={copy}
+            onlyIcon>
+            <CheckIcon
+                className={cn("absolute inset-1.5 size-4 transition-all", {
+                    "scale-0 opacity-0": !hasCopied,
+                })}
+            />
+            <ClipboardIcon
+                className={cn("absolute inset-1.5 size-4 transition-all", {
+                    "scale-0 opacity-0": hasCopied,
+                })}
+            />
+        </Button>
+    );
+};
--- website/components/docs/code-preview.tsx
+++ website/components/docs/code-preview.tsx
@@ -5,14 +5,16 @@ import { readFile } from "node:fs/promises";
 import { codeToHtml } from "shiki";
 
 import { ClientCodePreview } from "./client-code-preview";
+import { ClipboardButton } from "./clipboard-button";
 
 type Props = {
     path?: string;
     code?: string;
+    collapsible?: boolean;
     lang?: CodeToHastOptionsCommon["lang"];
 };
 
-export const CodePreview = async ({ path, code, lang = "tsx" }: Props) => {
+export const CodePreview = async ({ path, code, collapsible, lang = "tsx" }: Props) => {
     if (!code && path) code = await readFile(nodePath.join(process.cwd(), `/${path}.tsx`), "utf8");
 
     if (!code) {
@@ -23,7 +25,14 @@ export const CodePreview = async ({ path, code, lang = "tsx" }: Props) => {
         return text.replaceAll(/\{\s*\.\.\.props\s*}/g, "");
     };
 
+    const removeCustomTransformer = (text: string): string => {
+        const regexPattern = /\/\/ \[!code( [^\]]+)?]/g;
+        return text.replaceAll(regexPattern, "");
+    };
+
     const filteredText = filterText(code);
+
+    const copingText = removeCustomTransformer(filteredText);
 
     const html = await codeToHtml(filteredText, {
         lang: lang,
@@ -35,8 +44,11 @@ export const CodePreview = async ({ path, code, lang = "tsx" }: Props) => {
     });
 
     return (
-        <div className="mt-5 mb-4 overflow-hidden rounded">
-            <ClientCodePreview html={html} />
+        <div className="relative mt-5 mb-4 overflow-hidden rounded">
+            <ClientCodePreview html={html} collapsible={collapsible} />
+            <div className="absolute end-6 top-3 z-10">
+                <ClipboardButton text={copingText} />
+            </div>
         </div>
     );
 };
