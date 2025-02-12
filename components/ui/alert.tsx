26f4613ee9e76405262b87a8f9ad4646e8d8e64c
Denish Navadiya <denishnavadiya1@gmail.com>
2025-02-12 10:31:08 +0530

    feat: add success variant to alert

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -18,6 +18,7 @@ const alertTheme = tv({
             default: "",
             primary: "",
             secondary: "",
+            success: "",
             error: "",
         },
     },
@@ -39,6 +40,11 @@ const alertTheme = tv({
         },
         {
             variant: "solid",
+            color: "success",
+            className: "bg-success text-success-foreground",
+        },
+        {
+            variant: "solid",
             color: "error",
             className: "bg-error text-error-foreground",
         },
@@ -51,6 +57,11 @@ const alertTheme = tv({
             variant: "outline",
             color: "secondary",
             className: "border-secondary text-secondary",
+        },
+        {
+            variant: "outline",
+            color: "success",
+            className: "border-success text-success",
         },
         {
             variant: "outline",
@@ -71,6 +82,11 @@ const alertTheme = tv({
             variant: "soft",
             color: "secondary",
             className: "bg-secondary/10 text-secondary",
+        },
+        {
+            variant: "soft",
+            color: "success",
+            className: "bg-success/10 text-success",
         },
         {
             variant: "soft",
